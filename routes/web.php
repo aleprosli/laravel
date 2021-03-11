a3596aeae2901f32abc2259cf3add878e29476c7
ZER\USER <aliff.rosli96@gmail.com>
2021-03-11 12:15:36 +0800

    AL/UL View: Added

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,8 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
     Route::get('/leave', ['as' => 'leave', 'uses' => 'LeaveController@index']);
     Route::get('/mc', ['as' => 'mc', 'uses' => 'mcController@index']);
+    Route::get('/al', ['as' => 'al', 'uses' => 'alController@index']);
+    Route::get('/ul', ['as' => 'ul', 'uses' => 'ulController@index']);
 
     // Route::get('/leaves/typeleave', 'LeaveController');
 });
