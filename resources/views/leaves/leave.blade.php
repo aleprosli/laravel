6460a9de81430774ea6c4dd6e3a1381e0bd5cad4
ZER\USER <aliff.rosli96@gmail.com>
2021-03-15 10:39:31 +0800

    list apply leave:added:not function yet

--- resources/views/leaves/leave.blade.php
+++ resources/views/leaves/leave.blade.php
@@ -271,6 +271,21 @@
                     <div class="card-body">
                         <div class="row">
                             <div class="col">
+                                <h5 class="card-title text-uppercase text-muted mb-0">AL</h5>
+                                <span class="h2 font-weight-bold mb-0">Annual Leave</span>
+                                <a href="{{ route('al.index') }}" class="btn btn-sm btn-primary">ClickHere</a>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-6">
+                <div class="card card-stats mb-4 mb-xl-0">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col">
                                 <h5 class="card-title text-uppercase text-muted mb-0">UL</h5>
                                 <span class="h2 font-weight-bold mb-0">Unpaid Leave</span>
                                 {{-- <a href="{{ route('ul') }}" class="btn btn-sm btn-primary">ClickHere</a> --}}
@@ -282,35 +297,83 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xl-3 col-lg-6">
-                <div class="card card-stats mb-4 mb-xl-0">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col">
-                                <h5 class="card-title text-uppercase text-muted mb-0">AL</h5>
-                                <span class="h2 font-weight-bold mb-0">Annual Leave</span>
-                                <a href="{{ route('al.index') }}" class="btn btn-sm btn-primary">ClickHere</a>
-                            </div>
+            {{-- //admin --}}
 
-                        </div>
+            <div class="header bg-gradient-primary pb-8 pt-5 pt-md-8">
+                <div class="container-fluid">
+                    {{-- <div class="alert alert-danger" role="alert">
+                        <strong>This is a PRO feature!</strong>
+                      </div> --}}
+                    <div class="header-body">
+                        <!-- Card stats -->
 
                     </div>
                 </div>
-            </div>
-            {{-- //admin --}}
-            <div class="col-xl-3 col-lg-6">
-                <div class="card card-stats mb-4 mb-xl-0">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col">
-                                <h5 class="card-title text-uppercase text-muted mb-0">TL</h5>
-                                <span class="h2 font-weight-bold mb-0">Total Leave</span>
-                                <a href="" class="btn btn-sm btn-primary">ClickHere</a>
+                </div>
+            <div class="container-fluid mt--7">
+                <div class="row">
+                    <div class="col">
+                        <div class="card shadow">
+                            <div class="card-header border-0">
+                                <div class="row align-items-center">
+                                    <div class="col-8">
+                                        <h3 class="mb-0">List of Leaves </h3>
+                                    </div>
+                                    <div class="col-4 text-right">
+                                        <a href="" class="btn btn-sm btn-primary">Add user</a>
+                                    </div>
+                                </div>
                             </div>
 
+                            <div class="col-12">
+                                                    </div>
 
+                            <div class="table-responsive">
+                                <table class="table align-items-center table-flush">
+                                    <thead class="thead-light">
+                                        <tr>
+                                            <th scope="col">Name</th>
+                                            <th scope="col">Email</th>
+                                            <th scope="col">Creation Date</th>
+                                            <th scope="col">Type of Leave</th>
+                                            <th scope="col">Reason</th>
+                                            <th scope="col">Date</th>
+                                            <th scope="col">Status</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                                                            <tr>
+                                                <td>{{ auth()->user()->name }}</td>
+                                                <td>
+                                                    {{ auth()->user()->email }}</a>
+                                                </td>
+                                                <td>{{ auth()->user()->created_at }}</td>
+                                                <td>{{ auth()->user()->name }}</td>
+                                                <td>{{ auth()->user()->reason }}</td>
+                                                <td>{{ auth()->user()->date }}</td>
+                                                <td>{{ auth()->user()->name }}</td>
+
+
+                                                <td class="text-right">
+                                                    <div class="dropdown">
+                                                        <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                            <i class="fas fa-ellipsis-v"></i>
+                                                        </a>
+                                                        <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                                                                                                                    <a class="dropdown-item" href="">Edit</a>
+                                                                                                            </div>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="card-footer py-4">
+                                <nav class="d-flex justify-content-end" aria-label="...">
+
+                                </nav>
+                            </div>
                         </div>
-
                     </div>
                 </div>
             </div>
